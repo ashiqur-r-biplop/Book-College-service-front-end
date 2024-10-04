49b4bf3782cb4ed14b59e43187ac943fc6f48b62
ashiqur-r-biplop <biplops22@gmail.com>
2024-10-04 22:33:04 +0600

    added authprovider and firebase setup config and axios install

---
+++ src/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,97 @@
+/* eslint-disable react/prop-types */
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext(null);
+import {
+    GithubAuthProvider,
+    createUserWithEmailAndPassword,
+    getAuth,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    signOut,
+    updateProfile,
+} from "firebase/auth";
+import { app } from "../firebase/firebase.config";
+import axios from "axios";
+
+const githubProvider = new GithubAuthProvider();
+const AuthProvider = ({ children }) => {
+    const auth = getAuth(app);
+    const [Favorite, setFavorite] = useState([]);
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [reload, setReload] = useState(false);
+    const signUp = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+    const signInGoogle = (provider) => {
+        setLoading(true);
+        return signInWithPopup(auth, provider);
+    };
+    const signInGithub = () => {
+        setLoading(true);
+        return signInWithPopup(auth, githubProvider);
+    };
+    const login = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+    const logout = () => {
+        return signOut(auth);
+    };
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            // get and set token\
+            if (currentUser) {
+                // console.log("data");
+                axios
+                    .post("https://creativa-design-hub-server-site.vercel.app/jwt", {
+                        email: currentUser.email,
+                    })
+                    .then((data) => {
+                        localStorage.setItem("access-token", data.data.token);
+                        setLoading(false);
+                        setReload(false);
+                    });
+            } else {
+                localStorage.removeItem("access-token");
+                setLoading(false);
+            }
+            // setLoading(false);
+            // setReload(false);
+        });
+        return () => {
+            unSubscribe();
+        };
+    }, [reload]);
+    const ProfileUpdate = (name, PhotoUrl) => {
+        setLoading(true);
+        return updateProfile(auth.currentUser, {
+            displayName: name,
+            photoURL: PhotoUrl,
+        });
+    };
+    const authInfo = {
+        user,
+        signUp,
+        signInGoogle,
+        signInGithub,
+        login,
+        logout,
+        loading,
+        ProfileUpdate,
+        auth,
+        setFavorite,
+        Favorite,
+        setReload,
+    };
+
+    return (
+        <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,11 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { router } from './route/Routes.jsx';
+import AuthProvider from './AuthProvider/AuthProvider.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )

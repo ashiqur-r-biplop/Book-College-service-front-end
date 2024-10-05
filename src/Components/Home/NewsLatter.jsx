204737d6f75c364ccb071854b37ce7b82053ee7d
ashiqur-r-biplop <biplops22@gmail.com>
2024-10-05 14:58:42 +0600

    Added newslatter mobile issue solve

--- src/Components/Home/NewsLatter.jsx
+++ src/Components/Home/NewsLatter.jsx
@@ -12,8 +12,8 @@ const NewsLatter = () => {
                 backgroundPosition: "center",
                 clipPath: "polygon(0 0, 100% 12%, 100% 50%, 100% 100%, 0 100%, 0% 50%)",
             }}>
-                <div className='bg-[#757575a2] h-full md:p-[80px] pt-[40px] md:pt-[80px] flex flex-col md:flex-row justify-start md:justify-between items-start md:items-center gap-[20px]'>
-                    <p className='text-2xl md:w-[40%] text-[#fff]'>To Boost Science Learning, Start Early 4 Ways to Explore Science
+                <div className='bg-[#757575a2] h-full md:p-[80px] pt-[60px] md:pt-[80px] flex flex-col md:flex-row justify-start md:justify-between items-start md:items-center gap-[20px] px-[10px]'>
+                    <p className='md:text-2xl text-[18px] md:w-[40%] text-[#fff]'>To Boost Science Learning, Start Early 4 Ways to Explore Science
                         with Your Child</p>
                     <Link to={"/admission"}>
                         <button className='border border-[#FDE047] px-[30px] py-[5px] rounded-full text-[#FDE047] hover:bg-[#FDE047] hover:text-[#000] text-xl duration-300 transition-all'>Start Now</button>
